2775cee777d5bd04e6302e80621bea546618373c
Sanjog chaulagain <SC11@SC11-2.local>
2020-05-23 11:28:37 +0545

    Beginning

---
+++ spr.js
@@ -0,0 +1,25 @@
+const humanScore = document.querySelector(".hoomanscore");
+const computerScore = document.querySelector(".computerscore");
+const humanChoice = document.querySelector(".player-selection");
+const computerChoice = document.querySelector(".computer-selection");
+const tie = document.querySelector(".tie");
+const btns = document.querySelectorAll("button");
+const spr = ['ROCK', 'PAPER', 'SCISSSOR']
+let randomindex = Math.floor(Math.random() * spr.length);
+let randomchoice = spr[randomindex];
+
+
+
+btns[0].onclick = () => {
+    humanChoice.innerHTML = '';
+    humanChoice.innerHTML = 'PAPER';
+}
+btns[1].onclick = () => {
+    humanChoice.innerHTML = '';
+    humanChoice.innerHTML = 'ROCK';
+}
+btns[2].onclick = () => {
+    humanChoice.innerHTML = '';
+    humanChoice.innerHTML = 'SCISSOR';
+}
+
